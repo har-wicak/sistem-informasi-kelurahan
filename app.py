b8a3d48a349b988456aa0955681643d46e183cf8
Haryo Wicaksono <har.wicak@gmail.com>
2022-08-04 20:50:02 +0700

    added article page, bug fixes, minor changes

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    writer = db.Column(db.String(50), nullable=False)
+    content = db.Column(db.String(255), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -13,6 +27,27 @@ def index():
 def dashboard():
     if request.method == 'GET':
         return render_template('dashboard.html', page_title = "Dashboard")
+    else:
+        return "Unsupported Request Method"
+
+@app.route('/articles', methods=['GET', 'POST'])
+def articles():
+    if request.method == 'GET':
+        return render_template('articles.html', page_title = "Articles")
+    else:
+        return "Unsupported Request Method"
+
+@app.route('/profil', methods=['GET', 'POST'])
+def profil():
+    if request.method == 'GET':
+        return render_template('profil.html', page_title = "Profil")
+    else:
+        return "Unsupported Request Method"
+
+@app.route('/article', methods=['GET', 'POST'])
+def article():
+    if request.method == 'GET':
+        return render_template('ind_article.html', page_title = "Article")
     else:
         return "Unsupported Request Method"
 
